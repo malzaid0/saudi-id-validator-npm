9dab466fb572673f4db7d80896dd25580f059550
Mohammed Alzaid <m.al-zaid@live.com>
2022-04-04 06:34:32 +0300

    some code cleaning

--- src/idValidator.ts
+++ src/idValidator.ts
@@ -1,37 +1,37 @@
-import {easternToWesternNumbers, isNumeric} from "./numberHandler";
+import { easternToWesternNumbers, isNumeric } from "./numberHandler";
 
-const findBlockLength = (id: string, position: number) => {
-    return Number(id[position]) >= 5 ? 2 : 1
-};
-
-const evaluateBlock = (blockValue: number, id: string, position: number) => {
-    const trimValue = Number(id[position]);
-    if (blockValue === 1) {
-        return trimValue * 2;
-    }
-    const sum = (trimValue * 2).toString();
-    return Number(sum[0]) + Number(sum[1]);
+const evaluateDigit = (currentDigit: number) => {
+  if (currentDigit < 5) {
+    return currentDigit * 2;
+  }
+  const sum = (currentDigit * 2).toString();
+  return Number(sum[0]) + Number(sum[1]);
 };
 
 export const isValidSaudiID = (saudiID: string | number) => {
-    const stringID = easternToWesternNumbers(saudiID.toString());
+  const stringID = easternToWesternNumbers(saudiID.toString());
 
-    if (stringID.length !== 10 || !isNumeric(stringID) || !"12".includes(stringID[0])) {
-        return false;
-    }
+  if (
+    stringID.length !== 10 ||
+    !isNumeric(stringID) ||
+    !"12".includes(stringID[0])
+  ) {
+    return false;
+  }
 
-    let sum = 0
-    for (let position = 1; position < 8; position += 2) {
-        sum += Number(stringID[position])
+  let sum = 0;
+  for (let position = 0; position < stringID.length - 1; position++) {
+    if (position % 2 === 0) {
+      sum += evaluateDigit(Number(stringID[position]));
+    } else {
+      sum += Number(stringID[position]);
     }
-    for (let position = 0; position < stringID.length; position += 2) {
-        sum += evaluateBlock(findBlockLength(stringID, position), stringID, position);
-    }
+  }
 
-    let remainder = 10 - (sum % 10);
-    if (remainder === 10) {
-        remainder = 0;
-    }
+  let remainder = 10 - (sum % 10);
+  if (remainder === 10) {
+    remainder = 0;
+  }
 
-    return remainder === Number(stringID[9]);
+  return remainder === Number(stringID[9]);
 };
